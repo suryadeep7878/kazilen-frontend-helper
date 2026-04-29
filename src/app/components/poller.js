8e8932284b8066b66f3d2b8ddad0f76a8bba70d2
Rom <romitrajbind0@gmail.com>
2026-04-29 23:28:56 +0530

    !Automated message! Did work at Wed Apr 29 11:28:56 PM IST 2026

--- src/app/components/poller.js
+++ src/app/components/poller.js
@@ -1,26 +1,32 @@
-"use client";
+'use client'
 
 import { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { apiRequest } from "@/utils/api";
 
 export default function BackgroundTask() {
-	const userId = localStorage.getItem("userId");
-	const { data, error } = useQuery({
-		queryKey: ["background-sync"],
-		queryFn: () => apiRequest("/poll-this", "POST", { id: userId }),
-		refetchInterval: 10000,
-		refetchIntervalInBackground: true,
-	});
+  const { data, error } = useQuery({
+    queryKey: ["background-sync"],
+    queryFn: () => {
+      const userId = typeof window !== "undefined" ? localStorage.getItem("userId") : null;
+      
+      if (!userId) return null;
 
-	useEffect(() => {
-		if (data) {
-			console.log("Background Sync Successful:", data);
-		}
-		if (error) {
-			console.error("Background Sync Failed:", error);
-		}
-	}, [data, error]);
+      return apiRequest("/poll-this", "POST", { id: userId });
+    },
+    refetchInterval: 10000,
+    refetchIntervalInBackground: true,
+    enabled: typeof window !== "undefined" && !!localStorage.getItem("userId"),
+  });
 
-	return null;
+  useEffect(() => {
+    if (data) {
+      console.log("Background Sync Successful:", data);
+    }
+    if (error) {
+      console.error("Background Sync Failed:", error);
+    }
+  }, [data, error]);
+
+  return null;
 }
