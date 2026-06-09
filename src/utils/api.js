7c25c4b1c8db83ff5396aed756041d1959e8e37f
Rom <romitrajbind0@gmail.com>
2026-06-09 23:40:04 +0530

    !Automated message! Did work at Tue Jun  9 11:40:04 PM IST 2026

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.01:8000/api/helper";
+const BASE_URL = "https://kazilen-prod-899213799870.asia-south1.run.app/api/helper";
 
 export const apiRequest = async (endpoint, method = "Get", body = null) => {
 	const headers = {
